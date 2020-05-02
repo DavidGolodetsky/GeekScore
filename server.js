6698e5ce7ad58b34da5d851f7dfab6d4d462cdac
David Golodetsky <davyd.lito@gmail.com>
2020-05-02 23:53:00 +0300

    dist

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/teams', teams);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/dist'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
 }
 
 
