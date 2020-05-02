eb2cde0228655bbb5266b3b516a6c0f2d4316a92
David Golodetsky <davyd.lito@gmail.com>
2020-05-03 00:03:19 +0300

    dist

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/games', games);
 app.use('/teams', teams);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist'))
+    app.use(express.static('./client/dist'))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
 }
 
