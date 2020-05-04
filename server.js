083649e4982c46ef84a68f3a82489d71bb65da34
David Golodetsky <davyd.lito@gmail.com>
2020-05-04 22:47:13 +0300

    remote mongo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/users', users);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'))
-    app.get('/.*/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+    app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 
