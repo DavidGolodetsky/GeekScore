cd626a1328fbc888cd1a9003bdecd5592db16819
David Golodetsky <davyd.lito@gmail.com>
2020-05-05 01:02:28 +0300

    sw

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import vuetify from './plugins/vuetify';
-import './registerServiceWorker';
+// import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import VueOffline from 'vue-offline';
@@ -20,10 +20,3 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app')
-
-
-// const shouldSW = 'serviceWorker' in navigator
-// const prod = process.env.NODE_ENV === 'production'
-// if (shouldSW && prod) {
-//   navigator.serviceWorker.register('/service-worker.js')
-// }
