8cca04a3e22aab775b71f7ceb8442ff4b3ba3bd4
David Golodetsky <davyd.lito@gmail.com>
2020-05-10 19:46:48 +0300

    tests

---
+++ public/precache-manifest.dd73635a154344c67a6ee7adbdf9ef3c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "ddf97316683226c886da",
+    "url": "/css/app.8ae75868.css"
+  },
+  {
+    "revision": "6b7ddabb562a174853a9",
+    "url": "/css/chunk-vendors.7f917b0e.css"
+  },
+  {
+    "revision": "dbf032fb430df8e86c57",
+    "url": "/css/game.19fea253.css"
+  },
+  {
+    "revision": "3f6a9755badf81fb613d",
+    "url": "/css/signIn.47cb3f8f.css"
+  },
+  {
+    "revision": "2ebee26bf85ad899c477",
+    "url": "/css/signUp.47cb3f8f.css"
+  },
+  {
+    "revision": "34a367322833eb6663ce",
+    "url": "/css/team.2f1d9b97.css"
+  },
+  {
+    "revision": "28b1884765834816ce76",
+    "url": "/css/theAlert.d47b30c4.css"
+  },
+  {
+    "revision": "d0842fd57cabf7f4342d",
+    "url": "/css/theDialog.5392c0be.css"
+  },
+  {
+    "revision": "494203277662dfc7ef3c",
+    "url": "/css/theGoBack.c8467e0b.css"
+  },
+  {
+    "revision": "711fd041e2a8d79484ff971cfb00fb82",
+    "url": "/img/game.711fd041.jpg"
+  },
+  {
+    "revision": "61a7f72a5ebc154825c8dbf4e31ba952",
+    "url": "/img/logo.61a7f72a.svg"
+  },
+  {
+    "revision": "76152c6e19486636e96d1bca9c97e2a3",
+    "url": "/img/team.76152c6e.jpg"
+  },
+  {
+    "revision": "220f2e2382d63c098881811019847604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf97316683226c886da",
+    "url": "/js/app.2b70d530.js"
+  },
+  {
+    "revision": "6b7ddabb562a174853a9",
+    "url": "/js/chunk-vendors.4c34a4ba.js"
+  },
+  {
+    "revision": "dbf032fb430df8e86c57",
+    "url": "/js/game.37a38369.js"
+  },
+  {
+    "revision": "86d4cb4ce858ae559301",
+    "url": "/js/notFound.82936281.js"
+  },
+  {
+    "revision": "640d60be12dd69c9257f",
+    "url": "/js/resetPass.782525c6.js"
+  },
+  {
+    "revision": "3f6a9755badf81fb613d",
+    "url": "/js/signIn.914ce153.js"
+  },
+  {
+    "revision": "2ebee26bf85ad899c477",
+    "url": "/js/signUp.cab3fd86.js"
+  },
+  {
+    "revision": "34a367322833eb6663ce",
+    "url": "/js/team.d468e548.js"
+  },
+  {
+    "revision": "28b1884765834816ce76",
+    "url": "/js/theAlert.d6cf46cd.js"
+  },
+  {
+    "revision": "d0842fd57cabf7f4342d",
+    "url": "/js/theDialog.bd7e9cb0.js"
+  },
+  {
+    "revision": "494203277662dfc7ef3c",
+    "url": "/js/theGoBack.8dc41db3.js"
+  },
+  {
+    "revision": "9d40ba1bc3ac8fed7abe7eb3143dfc80",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "764fe6ac6ffdd3114f629cf0a85cba1e",
+    "url": "/site.webmanifest"
+  }
+]);
