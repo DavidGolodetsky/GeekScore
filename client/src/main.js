fd418947186025e7b3ee49324f6cbfaea0cb4e7e
David Golodetsky <davyd.lito@gmail.com>
2020-05-10 19:47:29 +0300

    sw

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import vuetify from './plugins/vuetify';
-// import './registerServiceWorker';
+import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import VueOffline from 'vue-offline';
--- client/vue.config.js
+++ client/vue.config.js
@@ -23,49 +23,49 @@ module.exports = {
       }
     }
   },
-  // pwa: {
-  //   workboxOptions: {
-  //     exclude: [/\.map$/, /_redirects/],
-  //     runtimeCaching: [{
-  //       urlPattern: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg))/g,
-  //       handler: 'networkFirst',
-  //       options: {
-  //         networkTimeoutSeconds: 20,
-  //         cacheName: 'img-cache',
-  //         cacheableResponse: {
-  //           statuses: [0, 200],
-  //         },
-  //       },
-  //     },
-  //     {
-  //       urlPattern: /^https:\/\/(fonts)/,
-  //       handler: 'networkFirst',
-  //       options: {
-  //         networkTimeoutSeconds: 20,
-  //         cacheName: 'fonts-cache',
-  //         cacheableResponse: {
-  //           statuses: [0, 200],
-  //         },
-  //       },
-  //     }]
-  //   }
-  // },
-  // configureWebpack: () => {
-  //   if (process.env.NODE_ENV === 'production') {
-  //     return {
-  //       plugins: [
-  //         new SWPrecache({
-  //           cacheId: "geekscore",
-  //           filepath: "service-worker.js",
-  //           staticFileGlobs: [
-  //             "public/index.html",
-  //             "public/site.webmanifest",
-  //             "dist/**/*.{js,css}"
-  //           ],
-  //           stripPrefix: "/"
-  //         })
-  //       ]
-  //     }
-  //   }
-  // }
+  pwa: {
+    workboxOptions: {
+      exclude: [/\.map$/, /_redirects/],
+      runtimeCaching: [{
+        urlPattern: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg))/g,
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 20,
+          cacheName: 'img-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/\/(fonts)/,
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 20,
+          cacheName: 'fonts-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      }]
+    }
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new SWPrecache({
+            cacheId: "geekscore",
+            filepath: "service-worker.js",
+            staticFileGlobs: [
+              "public/index.html",
+              "public/site.webmanifest",
+              "dist/**/*.{js,css}"
+            ],
+            stripPrefix: "/"
+          })
+        ]
+      }
+    }
+  }
 }
