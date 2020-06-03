4dc5e2f9dca50e3ce1dfb203dd7c0165c7f2c926
David Golodetsky <davyd.lito@gmail.com>
2020-06-03 23:13:42 +0300

    meta

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,48 +10,105 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomePage
+    component: HomePage,
+    // TODO: add vue-meta 
+    meta: {
+      title: "Geek Score - Homepage",
+      metaTags: {
+        name: "description",
+        content: "A starting page where described the idea of Geek Score"
+      }
+    }
   },
   {
     path: '/games',
     name: 'games',
     component: GameList,
-    meta: { requiresAuth: true },
+    meta: {
+      requiresAuth: true,
+      title: "Geek Score - Games page",
+      metaTags: {
+        name: "description",
+        content: "A list of user's games"
+      }
+    }
   },
   {
     path: '/game/:gameId',
     name: 'game',
     props: true,
     component: () => import(/* webpackChunkName: "game" */ '../views/GameDetails'),
-    meta: { requiresAuth: true },
+    meta: {
+      requiresAuth: true,
+      title: "Geek Score - Game page",
+      metaTags: {
+        name: "description",
+        content: "Game details page"
+      }
+    }
   },
   {
     path: '/team/:teamId',
     name: 'team',
     props: true,
     component: () => import(/* webpackChunkName: "team" */ '../views/TeamDetails'),
-    meta: { requiresAuth: true },
+    meta: {
+      requiresAuth: true,
+      title: "Geek Score - Team page",
+      metaTags: {
+        name: "description",
+        content: "Team details page"
+      }
+    }
   },
   {
     path: "/signin",
     name: "signIn",
-    component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn')
+    component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn'),
+    meta: {
+      title: "Geek Score - Sign in page",
+      metaTags: {
+        name: "description",
+        content: "A page for signing in"
+      }
+    }
   },
   {
     path: "/signup",
     name: "signUp",
-    component: () => import(/* webpackChunkName: "signUp" */ '../views/SignUp')
+    component: () => import(/* webpackChunkName: "signUp" */ '../views/SignUp'),
+    meta: {
+      title: "Geek Score - Sign up page",
+      metaTags: {
+        name: "description",
+        content: "A page for signing up"
+      }
+    }
   },
   {
     path: "/reset",
     name: "resetPass",
-    component: () => import(/* webpackChunkName: "resetPass" */ '../views/ResetPassword')
+    component: () => import(/* webpackChunkName: "resetPass" */ '../views/ResetPassword'),
+    meta: {
+      title: "Geek Score - Reset password page",
+      metaTags: {
+        name: "description",
+        content: "A page for resetting a password"
+      }
+    }
   },
   {
     path: "/404",
     alias: "*",
     name: "notFound",
-    component: () => import(/* webpackChunkName: "notFound" */ '../views/NotFound')
+    component: () => import(/* webpackChunkName: "notFound" */ '../views/NotFound'),
+    meta: {
+      title: "Geek Score - Not found page",
+      metaTags: {
+        name: "description",
+        content: "An error page"
+      }
+    }
   }
 ]
 
