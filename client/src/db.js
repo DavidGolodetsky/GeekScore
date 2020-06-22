5b35418bc4e18e022e45daee7b1ebb0f54880ff7
David Golodetsky <davyd.lito@gmail.com>
2020-06-23 01:24:05 +0300

    removed fb

--- client/src/db.js
+++ client/src/db.js
@@ -1,7 +1,5 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
 import * as firebaseui from 'firebaseui'
 import store from '@/store';
 import router from '@/router';
