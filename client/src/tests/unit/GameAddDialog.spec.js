c1d4c12c698e6244a83575b5db7cbc373823992f
David Golodetsky <davyd.lito@gmail.com>
2020-07-11 10:58:29 +0300

    removed type attr

--- client/src/tests/unit/GameAddDialog.spec.js
+++ client/src/tests/unit/GameAddDialog.spec.js
@@ -2,10 +2,15 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuetify from 'vuetify'
 import Vue from "vue"
 import GameAddDialog from "@/components/GameAddDialog";
+import TheDialog from "@/components/TheDialog";
 
 Vue.use(Vuetify)
 const localVue = createLocalVue()
 localVue.use(Vuetify)
+
+localVue.component(
+    'TheDialog', TheDialog
+)
 
 
 it('Game Add', async () => {
