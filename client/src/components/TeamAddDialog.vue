3cb77f6d5b0dc71e0ef4ea7a063629dcc3c6778e
apantidis <apantidis@lancom.gr>
2020-10-01 12:47:00 +0300

    fix issue #22 switch cooperative error

--- client/src/components/TeamAddDialog.vue
+++ client/src/components/TeamAddDialog.vue
@@ -12,38 +12,36 @@
       prepend-icon="mdi-account-group"
       label="Name"
     />
-    <template v-if="!isCoop">
-      <v-select
-        prepend-icon="mdi-account-multiple-plus"
-        :rules="selectRules"
-        :items="numberOfPlayers"
-        label="Number of players"
-        @change="setPlayers"
-      />
-      <v-text-field
-        v-for="(player, i) in players"
-        :key="i"
-        v-model.trim="player.name"
-        :readonly="isMe(player)"
-        :clearable="!isMe(player)"
-        prepend-icon="mdi-account"
-        :rules="playerRules"
-        :label="`Player #${i + 1}`"
-        @input="isUniqueName"
-      />
-      <v-switch
-        v-model="coop"
-        label="Cooperative"
-        color="secondary"
-        hide-details
-      />
-    </template>
+    <v-select
+      prepend-icon="mdi-account-multiple-plus"
+      :rules="selectRules"
+      :items="numberOfPlayers"
+      label="Number of players"
+      @change="setPlayers"
+    />
+    <v-text-field
+      v-for="(player, i) in players"
+      :key="i"
+      v-model.trim="player.name"
+      :readonly="isMe(player)"
+      :clearable="!isMe(player)"
+      prepend-icon="mdi-account"
+      :rules="playerRules"
+      :label="`Player #${i + 1}`"
+      @input="isUniqueName"
+    />
+    <v-switch
+      v-model="coop"
+      label="Cooperative"
+      color="secondary"
+      hide-details
+    />
   </the-dialog>
 </template>
 
 <script>
-import { requiredField, standardField } from '@/utils/validations';
-import { mapActions, mapGetters, mapState } from 'vuex';
+import { requiredField, standardField } from '@/utils/validations'
+import { mapActions, mapGetters, mapState } from 'vuex'
 
 export default {
   props: {
@@ -65,38 +63,38 @@ export default {
         .join(0)
         .split(0)
         .map((v, i) => i + 1),
-    };
+    }
   },
   computed: {
     ...mapState('games', ['games']),
     ...mapGetters('games', ['getGame']),
     game() {
-      return this.games ? this.getGame(this.gameId) : null;
+      return this.games ? this.getGame(this.gameId) : null
     },
     isCoop() {
-      return this.game ? this.game.coop || this.coop : null;
+      return this.game ? this.game.coop || this.coop : null
     },
   },
   methods: {
     ...mapActions('teams', ['createTeam']),
     setPlayers($ev) {
-      this.players = [{ name: 'Me' }];
+      this.players = [{ name: 'Me' }]
       for (let i = 1; i < $ev; i++) {
-        let player = { name: '' };
-        this.players.push(player);
+        let player = { name: '' }
+        this.players.push(player)
       }
-      if ($ev === 1) this.coop = true;
+      if ($ev === 1) this.coop = true
     },
     isMe(player) {
-      return player.name === 'Me';
+      return player.name === 'Me'
     },
     isUniqueName($ev) {
       let duplicatedPlayerName = this.players.filter(
         (player) => player.name === $ev
-      );
+      )
       const isDuplicated =
-        duplicatedPlayerName.length < 2 || 'This field should be unique';
-      this.playerRules = [...this.playerRules, isDuplicated];
+        duplicatedPlayerName.length < 2 || 'This field should be unique'
+      this.playerRules = [...this.playerRules, isDuplicated]
     },
     onSubmit() {
       const team = {
@@ -105,9 +103,9 @@ export default {
         name: this.name,
         coop: this.isCoop,
         players: this.players,
-      };
-      this.createTeam(team);
+      }
+      this.createTeam(team)
     },
   },
-};
+}
 </script>
