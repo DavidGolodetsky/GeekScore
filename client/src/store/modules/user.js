65cfa436b53abcff908fdd35ddd153aecf40dc56
Bernhard Wittmann <dev@bernhardwittmann.com>
2020-10-04 18:40:45 +0200

    fix user not existing on reload

--- client/src/store/modules/user.js
+++ client/src/store/modules/user.js
@@ -58,7 +58,9 @@ export default {
     autoSignIn({ commit }, payload) {
       commit('LOADING', true, { root: true });
       commit('SET_USER', { id: payload.uid });
-      router.push('/games');
+      if (router.currentRoute.path !== '/games') {
+        router.push('/games');
+      }
       commit('LOADING', false, { root: true });
     },
     async resetPassword({ commit }, payload) {
