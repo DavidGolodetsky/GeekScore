0b855d0314fb2099139b1adc4cd607388c9e97ce
mvogt <moritz.vogt@vogges.de>
2020-10-05 16:43:34 +0200

    Initial dockerize the app

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,70 +2,71 @@ const SWPrecache = require('sw-precache-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../public'),
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000'
-      }
+    outputDir: path.resolve(__dirname, '../public'),
+    devServer: {
+        proxy: {
+            '/api': {
+                secure: false,
+                target: 'http://localhost:3000'
+            }
+        }
+    },
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    chainWebpack: config => {
+        config.plugins.delete('prefetch');
+    },
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `@import "@/assets/styles/main.scss";`
+            }
+        }
+    },
+    pwa: {
+        workboxOptions: {
+            exclude: [/\.map$/, /_redirects/],
+            runtimeCaching: [{
+                urlPattern: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg))/g,
+                handler: 'networkFirst',
+                options: {
+                    networkTimeoutSeconds: 20,
+                    cacheName: 'img-cache',
+                    cacheableResponse: {
+                        statuses: [0, 200],
+                    },
+                },
+            },
+                {
+                    urlPattern: /^https:\/\/(fonts)/,
+                    handler: 'networkFirst',
+                    options: {
+                        networkTimeoutSeconds: 20,
+                        cacheName: 'fonts-cache',
+                        cacheableResponse: {
+                            statuses: [0, 200],
+                        },
+                    },
+                }]
+        }
+    },
+    configureWebpack: () => {
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [
+                    new SWPrecache({
+                        cacheId: "geekscore",
+                        filepath: "service-worker.js",
+                        staticFileGlobs: [
+                            "public/index.html",
+                            "public/site.webmanifest",
+                            "dist/**/*.{js,css}"
+                        ],
+                        stripPrefix: "/"
+                    })
+                ]
+            }
+        }
     }
-  },
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  chainWebpack: config => {
-    config.plugins.delete('prefetch');
-  },
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "@/assets/styles/main.scss";`
-      }
-    }
-  },
-  pwa: {
-    workboxOptions: {
-      exclude: [/\.map$/, /_redirects/],
-      runtimeCaching: [{
-        urlPattern: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg))/g,
-        handler: 'networkFirst',
-        options: {
-          networkTimeoutSeconds: 20,
-          cacheName: 'img-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/(fonts)/,
-        handler: 'networkFirst',
-        options: {
-          networkTimeoutSeconds: 20,
-          cacheName: 'fonts-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      }]
-    }
-  },
-  configureWebpack: () => {
-    if (process.env.NODE_ENV === 'production') {
-      return {
-        plugins: [
-          new SWPrecache({
-            cacheId: "geekscore",
-            filepath: "service-worker.js",
-            staticFileGlobs: [
-              "public/index.html",
-              "public/site.webmanifest",
-              "dist/**/*.{js,css}"
-            ],
-            stripPrefix: "/"
-          })
-        ]
-      }
-    }
-  }
 }
