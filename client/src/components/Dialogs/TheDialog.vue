1b5420fe2d31023abc895b1b3110c6bab0c2ab1e
DavidGo <davidgolodetsky@gmail.com>
2020-11-28 11:10:05 +0200

    cleanup

--- client/src/components/Dialogs/TheDialog.vue
+++ client/src/components/Dialogs/TheDialog.vue
@@ -12,40 +12,57 @@
     >
       <v-icon dark>mdi-{{ activatorIcon }}</v-icon>
     </v-btn>
-    <label :for="`activator_${activatorText}`" class="label_text">{{
+    <label
+      :for="`activator_${activatorText}`"
+      class="label_text"
+    >{{
       activatorText
     }}</label>
-    <v-dialog v-if="renderDialog" v-model="dialog" max-width="600">
+    <v-dialog
+      v-model="dialog"
+      max-width="600"
+    >
       <v-card class="the-dialog">
         <v-card-title>
           <h3 class="app-headline">{{ header }}</h3>
           <v-spacer />
-          <v-btn small aria-label="Close modal" fab text @click="close">
+          <v-btn
+            small
+            aria-label="Close modal"
+            fab
+            text
+            @click="close"
+          >
             <v-icon dark>mdi-close</v-icon>
           </v-btn>
         </v-card-title>
-        <v-form ref="form" v-model="valid" @submit.prevent="onSubmit">
+        <v-form
+          ref="form"
+          v-model="valid"
+          @submit.prevent="onSubmit"
+        >
           <v-card-text>
             <v-container>
               <slot />
             </v-container>
           </v-card-text>
-          <v-card-actions v-if="footer" class="footer">
+          <v-card-actions
+            v-if="footer"
+            class="footer"
+          >
             <v-spacer />
             <v-btn
               color="secondary darken-1"
               class="mr-2"
               outlined
               @click="close"
-              >Cancel</v-btn
-            >
+            >Cancel</v-btn>
             <v-btn
               color="secondary darken-1"
               outlined
               type="submit"
               :disabled="!valid"
-              >Submit</v-btn
-            >
+            >Submit</v-btn>
           </v-card-actions>
         </v-form>
       </v-card>
@@ -92,31 +109,20 @@ export default {
       default: true,
     },
   },
-  data() {
+  data () {
     return {
       dialog: false,
       valid: false,
-      renderDialog: false,
     };
   },
-  watch: {
-    dialog: {
-      handler(value) {
-        value
-          ? (this.renderDialog = true)
-          : setTimeout((this.renderDialog = false), 1000);
-      },
-      immediate: true,
-    },
-  },
   methods: {
-    onSubmit() {
+    onSubmit () {
       this.submitLogic();
       if (this.closeOnSubmit) {
         this.close();
       }
     },
-    close() {
+    close () {
       this.dialog = false;
     },
   },
