cd4de59392b676692aaf5e0671348b9c63f87887
DavidGo <davidgolodetsky@gmail.com>
2020-12-01 13:39:47 +0200

    validation fixed

--- client/src/utils/validations.js
+++ client/src/utils/validations.js
@@ -1,31 +1,22 @@
 export const requiredField = v => !!v || "Field is required"
 
-export const tooLongField = v => {
-  if (v) return !!(v.length <= 60) || "Field is too long"
-}
+export const tooLongField = v => v ? !!(v.length <= 60) || "Field is too long" : true
 
-export const tooShortField = v => {
-  if (v) return !!(v.length >= 2) || "Field is too short"
-}
+export const tooShortField = v => v ? !!(v.length >= 2) || "Field is too short" : true
 
-export const onlyWhitespaces = v => {
-  if (v) return !!(v.replace(/\s/g, "").length) || "Field contains only whitespaces"
-}
+export const onlyWhitespaces = v => v ? !!(v.replace(/\s/g, "").length) || "Field contains only whitespaces" : true
+
+export const emailField = v => v ? /.+@.+\..+/.test(v) || "E-mail must be valid" : true
+
+export const shortPassword = v => v ? !!(v.length >= 6) || "Password is too short" : true
 
 export const linkField = v => {
   if (v) {
     const link = (v && v.match(/(https?:\/\/[^\s]+)/g))
     return !!link || "Please provide a correct link";
-  }
+  } else true
 }
 
-export const emailField = v => {
-  if (v) return /.+@.+\..+/.test(v) || "E-mail must be valid"
-}
-
-export const shortPassword = v => {
-  if (v) return !!(v.length >= 6) || "Password is too short"
-}
 
 
 export const standardField = [tooLongField, tooShortField, onlyWhitespaces]
