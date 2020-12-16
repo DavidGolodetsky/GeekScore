00f7c67e6e8077f8dcf345f20af5e60d0c4e93ad
DavidGo <davidgolodetsky@gmail.com>
2020-12-16 22:25:02 +0200

    build

--- client/src/store/modules/games.js
+++ client/src/store/modules/games.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import { VueOfflineStorage } from "vue-offline";
 
 export default {
   namespaced: true,
@@ -7,100 +8,112 @@ export default {
     winRate: null,
   },
   mutations: {
-    CREATE_GAME (state, payload) {
+    CREATE_GAME(state, payload) {
       state.games
         ? (state.games = [...state.games, payload])
         : (state.games = [payload]);
     },
-    SET_GAMES (state, payload) {
+    SET_GAMES(state, payload) {
       state.games = payload;
     },
-    UPDATE_GAME (state, payload) {
+    UPDATE_GAME(state, payload) {
       const game = state.games.find((game) => game._id === payload._id);
       state.games = state.games.filter((game) => game._id !== payload._id);
       state.games.push({ ...game, ...payload });
     },
-    DELETE_GAME (state, payload) {
+    DELETE_GAME(state, payload) {
       const games = state.games.filter((game) => game._id !== payload);
       games.length ? (state.games = games) : (state.games = null);
     },
-    SET_WIN_RATE (state, payload) {
+    SET_WIN_RATE(state, payload) {
       state.winRate = payload;
     },
   },
   actions: {
-    async createGame ({ commit }, payload) {
+    async createGame({ commit }, payload) {
       try {
-        const user = localStorage.getItem('userId');
+        const user = localStorage.getItem("userId");
         const gamePayload = {
           ...payload,
           user,
-          imageUrl: '',
+          imageUrl: "",
           teams: [],
           favorite: false,
         };
-        const createdGame = await axios.post('/api/games', gamePayload);
-        commit('LOADING', true, { root: true });
-        commit('CREATE_GAME', { ...createdGame.data });
+        const createdGame = await axios.post("/api/games", gamePayload);
+        commit("LOADING", true, { root: true });
+        commit("CREATE_GAME", { ...createdGame.data });
       } catch (e) {
-        commit('ERROR', e, { root: true });
+        commit("ERROR", e, { root: true });
       } finally {
-        commit('LOADING', false, { root: true });
+        commit("LOADING", false, { root: true });
       }
     },
-    async loadGames ({ commit }) {
+    async loadGames({ commit }) {
       try {
-        const user = localStorage.getItem('userId');
-        commit('LOADING', true, { root: true });
-        const games = await axios.get('/api/games', { params: { user } });
-        commit('SET_GAMES', games.data);
+        const user = localStorage.getItem("userId");
+        commit("LOADING", true, { root: true });
+        const games = await axios.get("/api/games", { params: { user } });
+        VueOfflineStorage.set("games", games.data);
+        commit("SET_GAMES", games.data);
       } catch (e) {
-        commit('ERROR', e, { root: true });
+        commit("ERROR", e, { root: true });
       } finally {
-        commit('LOADING', false, { root: true });
+        commit("LOADING", false, { root: true });
       }
     },
-    async updateGame ({ commit }, payload) {
+    async loadGamesOffline({ commit }) {
       try {
-        commit('LOADING', true, { root: true });
+        commit("LOADING", true, { root: true });
+        const games = VueOfflineStorage.get("games", games);
+        commit("SET_GAMES", games);
+      } catch (e) {
+        commit("ERROR", e, { root: true });
+      } finally {
+        commit("LOADING", false, { root: true });
+      }
+    },
+    async updateGame({ commit }, payload) {
+      try {
+        commit("LOADING", true, { root: true });
         await axios.patch(`/api/games/${payload._id}`, payload);
-        commit('UPDATE_GAME', payload);
+        commit("UPDATE_GAME", payload);
       } catch (e) {
-        commit('ERROR', e, { root: true });
+        commit("ERROR", e, { root: true });
       } finally {
-        commit('LOADING', false, { root: true });
+        commit("LOADING", false, { root: true });
       }
     },
-    async deleteGame ({ commit }, payload) {
+    async deleteGame({ commit }, payload) {
       try {
-        commit('LOADING', true, { root: true });
+        commit("LOADING", true, { root: true });
         await axios.delete(`/api/games/${payload}`);
-        commit('DELETE_GAME', payload);
+        commit("DELETE_GAME", payload);
       } catch (e) {
-        commit('ERROR', e, { root: true });
+        commit("ERROR", e, { root: true });
       } finally {
-        commit('LOADING', false, { root: true });
+        commit("LOADING", false, { root: true });
       }
     },
-    async loadWinRate ({ commit }, payload) {
+    async loadWinRate({ commit }, payload) {
       try {
-        commit('LOADING', true, { root: true });
+        commit("LOADING", true, { root: true });
         let winRate = await axios.get(`/api/games/win-rate/${payload}`);
         if (winRate.data) {
           winRate = `${winRate.data}%`;
-          commit('SET_WIN_RATE', winRate);
+          commit("SET_WIN_RATE", winRate);
         } else {
-          commit('SET_WIN_RATE', null);
+          commit("SET_WIN_RATE", null);
         }
       } catch (e) {
-        commit('ERROR', e, { root: true });
+        commit("ERROR", e, { root: true });
       } finally {
-        commit('LOADING', false, { root: true });
+        commit("LOADING", false, { root: true });
       }
     },
   },
   getters: {
-    getGame (state) {
+    getGame(state) {
       return (gameId) => {
         if (state.games) return state.games.find((game) => game._id === gameId);
       };
