5ac580325237c53153791b68f4d99d7e2ba03e68
DavidGo <davidgolodetsky@gmail.com>
2021-01-08 22:07:56 +0200

    eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,14 +3,24 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/typescript'],
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    '@vue/typescript'
+  ],
   rules: {
     'vue/max-attributes-per-line': 0,
     'vue/singleline-html-element-content-newline': 0,
     'vue/multiline-html-element-content-newline': 0,
     'vue/html-closing-bracket-newline': 0,
     'vue/html-indent': 0,
-    'no-unused-vars': 1
+    'no-unused-vars': 1,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
