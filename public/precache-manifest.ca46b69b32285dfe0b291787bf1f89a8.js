9427ff679c1d04941e85c5bdc4ee2e72a769f122
David Golodetsky <david.golodetsky@lovecrafts.com>
2021-10-03 23:14:58 +0300

    release

---
+++ public/precache-manifest.ca46b69b32285dfe0b291787bf1f89a8.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "a70df4c68f58fe711595",
+    "url": "/css/app.ad48663b.css"
+  },
+  {
+    "revision": "807745f9eea721245ca0",
+    "url": "/css/chunk-vendors.cac5c2b6.css"
+  },
+  {
+    "revision": "368f461dd3c096cfc32f",
+    "url": "/css/game.0f2561de.css"
+  },
+  {
+    "revision": "135d9b1728813d5e815f",
+    "url": "/css/games.e33463d6.css"
+  },
+  {
+    "revision": "e3c00a553ee02915c5c7",
+    "url": "/css/game~games~profile~resetPass~signIn~signUp~team~tools.8d172ea0.css"
+  },
+  {
+    "revision": "c1f196acb0df54251a40",
+    "url": "/css/game~games~profile~team~tools.b30e3b24.css"
+  },
+  {
+    "revision": "65d6f119a5e15a268f0b",
+    "url": "/css/home.ac3162ad.css"
+  },
+  {
+    "revision": "b4bf6767dcab78de5474",
+    "url": "/css/signIn.52b56eae.css"
+  },
+  {
+    "revision": "f57cf9e2a09bbb7127ea",
+    "url": "/css/signUp.52b56eae.css"
+  },
+  {
+    "revision": "11d994549ff947f396df",
+    "url": "/css/team.ee78f5c8.css"
+  },
+  {
+    "revision": "9ff1cae614a5f3236b7c",
+    "url": "/css/tools.e3f18a8c.css"
+  },
+  {
+    "revision": "10a04d7dcada291ddf78d9a235b6846e",
+    "url": "/img/game.10a04d7d.webp"
+  },
+  {
+    "revision": "711fd041e2a8d79484ff971cfb00fb82",
+    "url": "/img/game.711fd041.jpg"
+  },
+  {
+    "revision": "bd0d12950c03979da9a36b58ff040103",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "5e967f377fd1619c1701c833e27cc749",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "daf15992e62e9d0662cbddcb4a16b8b2",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a74568f237dd7cd0c5ba74a3a2b9006e",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "8a032e5e90a4567f39c623c11b80bb90",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b11e10a3a8c0fc4f8ba7e687f3a1d510",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "0c15817562397ecc6a3481dc594f3eae",
+    "url": "/img/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "34b490c0c1f4337a050131b219574ef1",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "068bf6bf96dfe548398a15aa88079d34",
+    "url": "/img/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "db1a7729f920ce7536cb740ff4a043b3",
+    "url": "/img/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "0d2de909e92d22de3be319323f952085",
+    "url": "/img/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "93a42d746b5dd7d22bbbb7f77776936b",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "61a7f72a5ebc154825c8dbf4e31ba952",
+    "url": "/img/logo.61a7f72a.svg"
+  },
+  {
+    "revision": "76152c6e19486636e96d1bca9c97e2a3",
+    "url": "/img/team.76152c6e.jpg"
+  },
+  {
+    "revision": "bf4c6f1329fb97feba2a9cabc5a11233",
+    "url": "/img/team.bf4c6f13.webp"
+  },
+  {
+    "revision": "e797f1228c9254e4f24936c0d97ba37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70df4c68f58fe711595",
+    "url": "/js/app.a8b71209.js"
+  },
+  {
+    "revision": "807745f9eea721245ca0",
+    "url": "/js/chunk-vendors.1ad309f9.js"
+  },
+  {
+    "revision": "368f461dd3c096cfc32f",
+    "url": "/js/game.16ca8b94.js"
+  },
+  {
+    "revision": "135d9b1728813d5e815f",
+    "url": "/js/games.23ebb0f7.js"
+  },
+  {
+    "revision": "e3c00a553ee02915c5c7",
+    "url": "/js/game~games~profile~resetPass~signIn~signUp~team~tools.24de950f.js"
+  },
+  {
+    "revision": "c1f196acb0df54251a40",
+    "url": "/js/game~games~profile~team~tools.32a1defa.js"
+  },
+  {
+    "revision": "65d6f119a5e15a268f0b",
+    "url": "/js/home.ac72cc78.js"
+  },
+  {
+    "revision": "4860b937f407822a33dd",
+    "url": "/js/notFound.e1949e3d.js"
+  },
+  {
+    "revision": "1ca57ba12b225de285b4",
+    "url": "/js/profile.fb1896ec.js"
+  },
+  {
+    "revision": "caf3fa9300322c71580d",
+    "url": "/js/resetPass.20d7be15.js"
+  },
+  {
+    "revision": "b4bf6767dcab78de5474",
+    "url": "/js/signIn.8a40f017.js"
+  },
+  {
+    "revision": "f57cf9e2a09bbb7127ea",
+    "url": "/js/signUp.85e4c5ce.js"
+  },
+  {
+    "revision": "11d994549ff947f396df",
+    "url": "/js/team.fd41a2d1.js"
+  },
+  {
+    "revision": "9ff1cae614a5f3236b7c",
+    "url": "/js/tools.f7d9a96f.js"
+  },
+  {
+    "revision": "daaca8f45b4a0e7573d233c0ccded67a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "764fe6ac6ffdd3114f629cf0a85cba1e",
+    "url": "/site.webmanifest"
+  }
+]);
