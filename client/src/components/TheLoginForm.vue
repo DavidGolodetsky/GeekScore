89124431cf3f6b30af2302580fb580b47a01474d
David Golodetsky <david.golodetsky@lovecrafts.com>
2021-10-10 09:48:06 +0300

    Revert "test html validator"
    This reverts commit 40f5c3b07862329780e644531addfa1035b68659.

--- client/src/components/TheLoginForm.vue
+++ client/src/components/TheLoginForm.vue
@@ -1,7 +1,5 @@
 <template>
   <section class="the-login-form mt-12">
-    <h1>в</h1>
-    <h1>ф</h1>
     <v-card raised outlined dark class="auth-card">
       <v-form
         ref="form"
@@ -81,6 +79,14 @@
       </div>
     </v-card>
   </section>
+
+
+
+
+
+
+
+  
 </template>
 
 <script lang="ts">
