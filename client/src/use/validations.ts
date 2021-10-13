78fcf88e7d0dbd15af5650ec038650dec727b2ec
David Golodetsky <david.golodetsky@lovecrafts.com>
2021-10-13 21:52:23 +0300

    removed explicit types

--- client/src/use/validations.ts
+++ client/src/use/validations.ts
@@ -1,25 +1,24 @@
-export const requiredField = (v: string | number): boolean | string =>
-  !!v || 'Field is required'
+export const requiredField = (v: string | number) => !!v || 'Field is required'
 
-export const tooLongField = (v: string): boolean | string => {
+export const tooLongField = (v: string) => {
   const condition = !!(v?.length <= 100)
   const message = 'Field is too long'
   return v ? condition || message : true
 }
 
-export const tooShortField = (v: string): boolean | string => {
+export const tooShortField = (v: string) => {
   const condition = !!(v?.length >= 2)
   const message = 'Field is too short'
   return v ? condition || message : true
 }
 
-export const onlyWhitespaces = (v: string): boolean | string => {
+export const onlyWhitespaces = (v: string) => {
   const condition = !!v?.replace(/\s/g, '').length
   const message = 'Field contains only whitespaces'
   return v ? condition || message : true
 }
 
-export const emailField = (v: string): boolean | string => {
+export const emailField = (v: string) => {
   const slicedV = v?.slice(0, 100)
   // skipcq: JS-S001
   const condition = /.+@.+\..+/.test(slicedV)
@@ -27,13 +26,13 @@ export const emailField = (v: string): boolean | string => {
   return v ? condition || message : true
 }
 
-export const shortPassword = (v: string): boolean | string => {
+export const shortPassword = (v: string) => {
   const condition = !!(v?.length >= 6)
   const message = 'Password is too short'
   return v ? condition || message : true
 }
 
-export const linkField = (v: string): boolean | string => {
+export const linkField = (v: string) => {
   const condition = !!v?.match(/(https?:\/\/[^\s]+)/g)
   const message = 'Please provide a correct link'
   return v ? condition || message : true
