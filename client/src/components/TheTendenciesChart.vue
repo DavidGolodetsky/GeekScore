11ec94bec04dcc0a471ccb3fedcdb40f5378360f
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2021-10-24 00:11:20 -0300

    Refactor tendencies chart component to composition api

--- client/src/components/TheTendenciesChart.vue
+++ client/src/components/TheTendenciesChart.vue
@@ -1,10 +1,17 @@
-<script>
-import { Line } from 'vue-chartjs'
+<template>
+  <LineChart :chart-data="chartData" :options="chartOptions" />
+</template>
 
+<script lang="ts">
+import { defineComponent, computed } from '@vue/composition-api';
+import { LineChart } from 'vue-chart-3'
+import { Chart, ChartData, ChartOptions, registerables } from "chart.js";
 
-export default {
-  name: "TheTendenciesChart",
-  extends: Line,
+Chart.register(...registerables)
+
+export default defineComponent({
+  name: 'TheTendenciesChart',
+  components: { LineChart },
   props: {
     team: {
       type: Object,
@@ -15,70 +22,52 @@ export default {
       required: true
     }
   },
-  computed: {
-    playersStat () {
-      const groupedByDate = this.rounds.reduce((r, a) => {
+  setup(props) {
+    const playersStat = computed(() => {
+      const groupedByDate: any = props.rounds.reduce((r: any, a: any) => {
         const month = new Date(a.date).toLocaleString('default', { month: 'short' })
         r[month] = r[month] || [];
         r[month].push(a);
         return r;
       }, Object.create(null));
 
-      const players = this.team.players.map(player => player.name.toLowerCase());
+      const players: any = props.team.players.map((player: any) => player.name.toLowerCase());
 
       const stat = []
 
       for (let date in groupedByDate) {
-        const dateObject = { date }
-        players.forEach(player => {
-          const playerRes = groupedByDate[date].filter(round => round.winner === player)
+        const dateObject: any = { date }
+        players.forEach((player: any) => {
+          const playerRes: any = groupedByDate[date].filter((round: any) => round.winner === player)
           dateObject[player] = playerRes.length
         })
         stat.push(dateObject)
       }
       return stat
-    },
-    chartdata () {
-      return {
-        // TODO:refactor that horror
-        labels: this.playersStat.map(data => data.date),
-        datasets: this.team.players.map(player => {
-          const victories = this.playersStat.map(data => data[player.name.toLowerCase()])
-          let top = Math.round(Math.max.apply(null, victories) + 10 / 10) + 5;
-          const r = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-          return {
-            label: player.name,
-            borderColor: `rgba(${r(0, 225)}, ${r(0, 225)}, ${r(0, 225)}, 0.4)`,
-            backgroundColor: 'transparent',
-            data: [...victories, top]
-          }
-        })
-      }
-    },
-  },
-  watch: {
-    rounds () {
-      this.updateChart()
-    }
-  },
-  mounted () {
-    this.setChart()
-  },
-  methods: {
-    setChart () {
-      this.renderChart(
-        this.chartdata,
-        {
-          responsive: true,
-          maintainAspectRatio: false
+    });
+
+    const chartData = computed<ChartData<'line'>>(() => ({
+      // TODO:refactor that horror
+      labels: playersStat.value.map((data: any) => data.date),
+      datasets: props.team.players.map((player: any) => {
+        const victories: number[] = playersStat.value.map((data: any) => data[player.name.toLowerCase()])
+        let top: number = Math.round(Math.max.apply(null, victories) + 10 / 10) + 5;
+        const r = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
+        return {
+          label: player.name,
+          borderColor: `rgba(${r(0, 225)}, ${r(0, 225)}, ${r(0, 225)}, 0.4)`,
+          backgroundColor: 'transparent',
+          data: [...victories, top]
         }
-      )
-    },
-    updateChart () {
-      if (!this.$data._chart) return
-      this.$data._chart.data = this.chartdata;
-      this.$data._chart.update();
-    },
+      })
+    }));
+
+    const chartOptions: ChartOptions<'line'> = {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+    return { chartData, chartOptions };
   }
-}
+})
 </script>
