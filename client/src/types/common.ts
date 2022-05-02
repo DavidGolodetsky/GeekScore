86cdd94ade67e5b42fdf8c18a5f081a559653135
David Golodetsky <david.golodetsky@lovecrafts.com>
2022-05-02 23:41:34 +0300

    test

--- client/src/types/common.ts
+++ client/src/types/common.ts
@@ -2,14 +2,12 @@ export interface Error {
   message: string;
   code: string;
 }
-
 export interface DemoStep {
   title: string;
   text: string;
   color: string;
   icon: string;
 }
-
 export interface ToolsDialogTab {
   name: string;
   component: string;
